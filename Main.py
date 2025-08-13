04118fec490e92240b67de0955c884aa58a9f08e
MikaelLopesDev <mikaelslopesdev@gmail.com>
2025-08-13 17:16:07 -0300

    refactor: changed the data sctruct to list instead of dataframe

--- Main.py
+++ Main.py
@@ -3,38 +3,36 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import pandas as pd
 
-class Spotify:
+class AiAgentSpotifySongsRecommendation:
     def __init__(self):
         self.scope = "user-top-read"
-        # Inicialize o objeto SpotifyOAuth com o scope correto
+
         load_dotenv()
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=self.scope))
-        self.df_my_top_artist = pd.DataFrame(columns=['Name', 'Genders'])
-        self.df_my_top_tracks = pd.DataFrame(columns=['Name_song', 'Name_artist'])
+        self.list_my_top_artist = []
+        self.list_my_top_tracks = []
 
 
     def get_df_top_artist(self,limit,offset,time_range ):
         top_artist = self.sp.current_user_top_artists(limit=limit, offset=offset, time_range=time_range)
         for artist in top_artist['items']:
-            new_row = pd.DataFrame([{'Name': artist['name'], 'Genders': ','.join(artist['genres'])}])
-            self.df_my_top_artist = pd.concat([self.df_my_top_artist,new_row], ignore_index=True)
-        return self.df_my_top_artist
+            self.list_my_top_artist.append(f"Name artist: {artist['name']} and his genders are {','.join(artist['genres'])}")
+        return self.list_my_top_artist
 
 
     def get_df_top_tracks(self,limit,offset,time_range):
-        try:
-            top_tracks = self.sp.current_user_top_tracks(limit=limit,offset=offset,time_range=time_range)
-            for track in top_tracks['items']:
-                new_row = pd.DataFrame([{'Name': track['name'], 'Name_artist': track['artists'][0]['name']}])
-                self.df_my_top_tracks = pd.concat([self.df_my_top_tracks, new_row], ignore_index=True)
-            return self.df_my_top_tracks
-        except Exception as e:
-            print(f"Ocorreu um erro: {e}")
+
+        top_tracks = self.sp.current_user_top_tracks(limit=limit,offset=offset,time_range=time_range)
+        for track in top_tracks['items']:
+            self.list_my_top_tracks.append(f"Name track: {track['name']} and his main singer is {track['artists'][0]['name']}")
+        return self.list_my_top_tracks
+
+
 
 
 if __name__ == "__main__":
-    spotify = Spotify()
-    my_top_artirst = spotify.get_df_top_artist(limit=10,offset=0,time_range="short_term")
+    spotify = AiAgentSpotifySongsRecommendation()
+    my_top_artist = spotify.get_df_top_artist(limit=10,offset=0,time_range="short_term")
     my_top_tracks = spotify.get_df_top_tracks(limit=10,offset=0,time_range="short_term")
     print("ok")
 
